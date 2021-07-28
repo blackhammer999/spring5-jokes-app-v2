093e7e053c72de279ff7e568ca353dd5e5b39bbb
blackhammer999 <disastercore0001@gmail.com>
2021-07-28 21:46:11 +0530

    Service Implementation done. Closes #3.

---
+++ src/main/java/spring5guru/tutorial/udemy/spring5jokesappv2/service/jokesService.java
@@ -0,0 +1,7 @@
+package spring5guru.tutorial.udemy.spring5jokesappv2.service;
+
+public interface jokesService {
+
+  String getJokes();
+
+}
---
+++ src/main/java/spring5guru/tutorial/udemy/spring5jokesappv2/service/jokesServiceImpl.java
@@ -0,0 +1,18 @@
+package spring5guru.tutorial.udemy.spring5jokesappv2.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class jokesServiceImpl implements
+    jokesService {
+  ChuckNorrisQuotes quotes = null;
+
+  public jokesServiceImpl() {
+    this.quotes = new ChuckNorrisQuotes();
+  }
+
+  @Override
+  public String getJokes() {
+    return quotes.getRandomQuote();
+  }
+
+}
